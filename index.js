91f6dee9605b0ef3a720a1fca7475bd7d8df84f3
AndJKel <50556546+AndJKel@users.noreply.github.com>
2024-08-05 14:39:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Mobile friendly. Information for search obtained from: https://stackoverflow.com/questions/77018595/filter-html-list-based-on-inputs-value
+
+// Starts mobile branch of files.
+if (screen.width <= 1000) {
+document.location = "mobile.html";}
